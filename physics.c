0c64a2d014f1f53956a8b9cbcf2d8453ac259961
Chenyang Yuan <yuanchenyang@gmail.com>
2013-11-05 19:09:09 -0800

    Added physics.c

--- physics.c
+++ physics.c
@@ -3,8 +3,6 @@
 #define OPTIMIZE_ROM_CALLS
 #include <tigcclib.h>
 
-#define BITMAP_SIZE (BITMAP_HDR_SIZE + HEIGHT*HEIGHT/8)
-
 #define RADIUS 5
 #define TIMESTEP 0.2
 #define SIM_TIME 500
@@ -22,31 +20,37 @@ typedef struct {
   float y;
 } Vector;
 
+// Draws a circle at positon v
 void updateCircle(const Vector *v, short Attr) {
   int x = (int) (v->x + 0.5);
   int y = (int) (v->y + 0.5);
   DrawClipEllipse(x, y, RADIUS, RADIUS, SCREEN, Attr);
 }
 
+// v <- v + a
 void addVector(Vector *v, const Vector *a) {
   v->x += a->x;
   v->y += a->y;
 }
 
+// v <- v - a
 void subVector(Vector *v, const Vector *a) {
   v->x -= a->x;
   v->y -= a->y;
 }
 
+// dest <- src * c
 void mulConstVector(Vector *dest, Vector *src, float c) {
   dest->x = src->x * c;
   dest->y = src->y * c;
 }
 
+// |v|
 float lengthVector(Vector *v) {
   return sqrt((v->x)*(v->x) + (v->y)*(v->y));
 }
 
+// For debugging
 void printVector(Vector *v) {
   printf("(%f, %f)\n", v->x, v->y);
 }
@@ -65,7 +69,9 @@ void updateLinearGravity(Vector *r, Vector *v) {
 }
 
 void updateRadialGravity(Vector *r, Vector *v) {
+  // Change vg to change the source of gravity!
   Vector vg = {LCD_WIDTH / 2, LCD_HEIGHT / 2};
+
   Vector dr = {0, 0}, dv = {0, 0}, force = {0, 0};
   float len, tmp;
 
@@ -74,6 +80,7 @@ void updateRadialGravity(Vector *r, Vector *v) {
   len = lengthVector(&vg);
   tmp = GRAVATIONAL_CONSTANT * GRAVITY / (len * len * len);
   if (tmp > 1) {
+    // Hack such that particle don't get a huge boost near gravitational source
     tmp = 0;
   }
   force.x = tmp * vg.x;
@@ -86,4 +93,21 @@ void updateRadialGravity(Vector *r, Vector *v) {
   // Increment position
   mulConstVector(&dr, v, TIMESTEP);
   addVector(r, &dr);
+}
+
+void initialize(Vector *r, Vector *v) {
+  *r = (Vector) {80, 10};  // Modify these to change initial position and
+  *v = (Vector) {10, 1};   // velocity!
+  clrscr();
+  updateCircle(r, A_NORMAL);
+}
+
+_main() {
+  Vector r, v, prevr;
+  float t;
+  int k;
+  void (*updateFunc)(Vector*, Vector*) =
+    (void (*)(Vector*, Vector*)) updateLinearGravity;
+  initialize(&r, &v);
+  // 1. *** YOUR CODE HERE ***
 }
